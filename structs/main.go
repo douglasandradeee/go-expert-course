3af17c1a6090caa269179e9d1ea6472d9854c16d
Douglas Borges Andrade <dba12@hotmail.com>
2023-01-09 00:40:36 -0300

    metodos_basico

--- structs/main.go
+++ structs/main.go
@@ -2,20 +2,41 @@ package main
 
 import "fmt"
 
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
 type Cliente struct {
 	Nome  string
 	Idade int
+	Endereco
 	Ativo bool
+}
+
+// Desativar - Método do objeto cliente que permite desativar.
+func (c *Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("\nCliente %s foi desativado.", c.Nome)
 }
 
 func main() {
 
 	cliente1 := Cliente{Nome: "Gabriela", Idade: 29, Ativo: true}
 
-	fmt.Printf("Nome: %s Idade: %d Ativo: %t", cliente1.Nome, cliente1.Idade, cliente1.Ativo)
+	//fmt.Printf("Nome: %s Idade: %d Ativo: %t", cliente1.Nome, cliente1.Idade, cliente1.Ativo)
 	fmt.Printf("\ncliente_1: %+v", cliente1)
 
-	cliente1.Ativo = false
+	cliente1.Desativar()
+	fmt.Printf("\ncliente_1_desa: %+v", cliente1)
+
+	cliente1.Logradouro = "Rua aleatória"
+	cliente1.Numero = 15
+	cliente1.Cidade = "Salvador"
+	cliente1.Estado = "Bahia"
+
 	fmt.Printf("\ncliente_1: %+v", cliente1)
 
 }
