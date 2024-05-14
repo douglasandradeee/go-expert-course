bf239041a1279ef524b7e3e3bcc955867d54461d
Douglas Borges Andrade <dba12@hotmail.com>
2024-05-13 23:28:54 -0300

    starting_to_work_with_data_stream

--- gRPC/internal/service/category.go
+++ gRPC/internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/douglasandradeee/go-expert-course/gRPC/internal/database"
 	"github.com/douglasandradeee/go-expert-course/gRPC/internal/pb"
@@ -69,4 +70,30 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetReq
 	return &pb.CategoryResponse{
 		Category: categoryResponse,
 	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+
+	}
 }
