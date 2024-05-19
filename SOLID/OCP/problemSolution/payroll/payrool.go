f757b3c8d78a158c3732f51aebc1e0c17fbda91a
Douglas Borges Andrade <dba12@hotmail.com>
2024-05-19 04:16:32 -0300

    adjust

--- SOLID/OCP/problemSolution/payroll/payrool.go
+++ SOLID/OCP/problemSolution/payroll/payrool.go
@@ -1,6 +1,6 @@
 package payroll
 
-import "github.com/douglasandradeee/go-expert-course/SOLID/OCP/employee"
+import "github.com/douglasandradeee/go-expert-course/SOLID/OCP/problemSolution/employee"
 
 func Payroll(e employee.Employee) float64 {
 	return e.CalculateSalary()
--- SOLID/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/douglasandradeee/go-expert-course/SOLID
-
-go 1.21.4
