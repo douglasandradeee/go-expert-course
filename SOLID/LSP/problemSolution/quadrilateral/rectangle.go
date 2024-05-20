c454c50dca4fda2374abe9895a09c59a2c5d2de8
Douglas Borges Andrade <dba12@hotmail.com>
2024-05-20 01:02:06 -0300

    update

--- SOLID/LSP/problemSolution/quadrilateral/rectangle.go
+++ SOLID/LSP/problemSolution/quadrilateral/rectangle.go
@@ -2,34 +2,34 @@ package quadrilateral
 
 type Rectangle struct {
 	Area
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 type RectangleInterface interface {
 	GetWidth() float64
 	GetHeight() float64
 
-	setWidth(width float64)
-	setHeight(height float64)
+	SetWidth(width float64)
+	SetHeight(height float64)
 }
 
 func (r *Rectangle) GetArea() float64 {
-	return r.width * r.height
+	return r.Width * r.Height
 }
 
 func (r *Rectangle) GetWidth() float64 {
-	return r.width
+	return r.Width
 }
 
 func (r *Rectangle) GetHeight() float64 {
-	return r.height
+	return r.Height
 }
 
 func (r *Rectangle) SetWidth(width float64) {
-	r.width = width
+	r.Width = width
 }
 
 func (r *Rectangle) SetHeight(height float64) {
-	r.height = height
+	r.Height = height
 }
--- SOLID/LSP/problemSolution/quadrilateral/square.go
+++ SOLID/LSP/problemSolution/quadrilateral/square.go
@@ -1,23 +1,23 @@
 package quadrilateral
 
-type SquareIterface interface {
+type SquareInterface interface {
 	Area
 	GetSize() float64
 	SetSize(size float64)
 }
 
 type Square struct {
-	size float64
+	Size float64
 }
 
 func (s *Square) GetArea() float64 {
-	return s.size * s.size
+	return s.Size * s.Size
 }
 
 func (s *Square) GetSize() float64 {
-	return s.size
+	return s.Size
 }
 
 func (s *Square) SetSize(size float64) {
-	s.size = size
+	s.Size = size
 }
